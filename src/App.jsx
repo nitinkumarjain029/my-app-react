05bf8c9f3cf1f22bfd3d9119d61e22f032fdb238
david <nitin.porwal90@gmail.com>
2025-05-25 17:58:39 +0530

    change name

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite Nitin + React</h1>
+      <h1>Vite Nitin + Demo React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
